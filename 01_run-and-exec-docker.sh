0115bc9d7094c4c54e079c8c251c396dd66857b9
Ivor Hung <ihung@rbbn.com>
2020-01-14 18:17:40 +0800

    Update 01_run-and-exec-docker.sh

--- 01_run-and-exec-docker.sh
+++ 01_run-and-exec-docker.sh
@@ -2,6 +2,8 @@
 MYDOCKER="ihung-netopeer2"
 #docker run -e AUTHORIZED_KEYS=/root/.ssh/authorized_keys -dit --publish 22222:22 --name $MYDOCKER  $MYDOCKER:latest
 docker run -dit --publish 830:830 --name $MYDOCKER  $MYDOCKER:latest
+#or
+#docker run -dit --publish 830:830 --name ihung-netopeer2 ivor911/ihung-netopeer2:latest
 docker exec -it $MYDOCKER /bin/bash
 
 #######################################################################
