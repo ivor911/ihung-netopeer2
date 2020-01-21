0f2143329abfb0a07d756752741712ed39965fae
Ivor Hung <ihung@rbbn.com>
2020-01-21 15:46:43 +0800

    Fixed MYTAG

--- 00_build-docker-image.sh
+++ 00_build-docker-image.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 MYDOCKER_REPO="ihung-netopeer2"
-MYTAG=`git branch | grep \* | cut -d ' ' -f2-`
+#MYTAG=`git branch | grep \* | cut -d ' ' -f2-`
+MYTAG="kea-port"
 MYDOCKER_NAME="ihung-netopeer2"
 docker build --rm --no-cache -t $MYDOCKER_REPO:$MYTAG . 
