1b131d6fb39dfed88ac2965ef7fb924a7a4cbc92
Ivor Hung <ihung@rbbn.com>
2020-02-14 15:30:53 +0800

    Add script to run docker in privileged, net host mode.

---
+++ 02_run-and-exec-docker_priv-netHost.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+MYDOCKER_REPO="ihung-netopeer2"
+#MYTAG=`git branch | grep \* | cut -d ' ' -f2-`
+MYTAG="yang-port"
+MYDOCKER_NAME="ihung-netopeer2"
+#docker run -e AUTHORIZED_KEYS=/root/.ssh/authorized_keys -dit --publish 22222:22 --name $MYDOCKER  $MYDOCKER:latest
+#docker run -dit --publish 830:830 --name $MYDOCKER_NAME  $MYDOCKER_REPO:$MYTAG
+docker run -dit --privileged --net=host --name $MYDOCKER_NAME  $MYDOCKER_REPO:$MYTAG
+#or
+#docker run -dit --publish 830:830 --name ihung-netopeer2 ivor911/ihung-netopeer2:latest
+docker exec -it $MYDOCKER_NAME /bin/bash
+
+#######################################################################
+# After in docker, run:
+# root@c89b41fecc1d:~/netopeer2-all-build# netopeer2-server -d -v 2
+#######################################################################
+# In another docker shell, run:
+# root@c89b41fecc1d:~/netopeer2-all-build# netopeer2-cli
+# > auth pref
+# > auth pref password 0
+# > auth pref interactive 0
+# > auth keys
+# > auth keys add /root/.ssh/id_rsa.pub /root/.ssh/id_rsa
+# > connect
+
